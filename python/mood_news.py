198c358be5162836510f649966ee9838f6c95959
Shishir Bhat <shishirvbhat@gmail.com>
2018-03-03 11:47:57 -0500

    working controller

--- python/mood_news.py
+++ python/mood_news.py
@@ -4,23 +4,47 @@ import Parser
 
 def get_happy_articles():
     a = Parser.get_urls_with_threshold(bt.overall(), "Joy")
+    b = set()
     if len(a) < 5:
         b = Parser.get_urls_with_threshold(bt.health(), "Joy", 5 - len(a))
+    return set(a).union(b)
 
 
 def get_sad_articles():
-    return Parser.get_urls_with_threshold(bt.overall(), "Sadness")
+    a = Parser.get_urls_with_threshold(bt.overall(), "Sadness")
+    b = set()
+    if len(a) < 5:
+        b = Parser.get_urls_with_threshold(bt.business(), "Sadness", 5 - len(a))
+    return set(a).union(b)
 
 
 def get_angry_articles():
-    return Parser.get_urls_with_threshold(bt.overall(), "Anger")
+    a = Parser.get_urls_with_threshold(bt.overall(), "Anger")
+    b = set()
+    if len(a) < 5:
+        b = Parser.get_urls_with_threshold(bt.business(), "Anger", 5 - len(a))
+    return set(a).union(b)
 
 
 def get_fear_articles():
-    return Parser.get_urls_with_threshold(bt.overall(), "Fear")
+    a = Parser.get_urls_with_threshold(bt.overall(), "Fear")
+    b = set()
+    if len(a) < 5:
+        b = Parser.get_urls_with_threshold(bt.business(), "Fear", 5 - len(a))
+    return set(a).union(b)
 
 
 def get_curious_articles():
-    return Parser.get_urls_with_threshold(bt.overall(), "Analytical")
+    a = Parser.get_urls_with_threshold(bt.overall(), "Analytical")
+    b = set()
+    if len(a) < 5:
+        b = Parser.get_urls_with_threshold(bt.business(), "Analytical", 5 - len(a))
+    return set(a).union(b)
 
-def
+
+def get_article_image(url):
+    from newspaper import Article
+    a = Article(url)
+    a.download()
+    a.parse()
+    return a.top_image
