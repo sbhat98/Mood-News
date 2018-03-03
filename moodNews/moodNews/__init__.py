b81b42deb172bc0f81e2ce927172638eca837f57
sd7776 <31454014+sd7776@users.noreply.github.com>
2018-03-03 17:55:16 -0500

    Flask Implementation
    I have implemented the flask framework for the website.

---
+++ moodNews/moodNews/__init__.py
@@ -0,0 +1,1 @@
+from .moodNews import app
---
+++ moodNews/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='moodNews',
+    packages=['moodNews'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
