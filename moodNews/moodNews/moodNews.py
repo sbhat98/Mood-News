fad106abd7db9c17da7db68c7f0e7a1c75666a87
sd7776 <31454014+sd7776@users.noreply.github.com>
2018-03-03 20:39:27 -0500

    updated the websites with flask

--- moodNews/moodNews/moodNews.py
+++ moodNews/moodNews/moodNews.py
@@ -5,9 +5,17 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/api')
+def api():
+    return render_template('api.html')
 
 if __name__ == "__main__":
     app.run()
